98b5ef2bfc24fb361e3bace688820e3e1bd0a9a7
ma jun chang <swiftma@sina.com>
2017-09-21 21:48:04 +0800

    update atomic integer

--- src/shuo/laoma/concurrent/c70/AtomicIntegerDemo.java
+++ src/shuo/laoma/concurrent/c70/AtomicIntegerDemo.java
@@ -8,15 +8,14 @@ public class AtomicIntegerDemo {
 	static class Visitor extends Thread {
 		@Override
 		public void run() {
-			for (int i = 0; i < 100; i++) {
+			for (int i = 0; i < 1000; i++) {
 				counter.incrementAndGet();
-				Thread.yield();
 			}
 		}
 	}
 
 	public static void main(String[] args) throws InterruptedException {
-		int num = 100;
+		int num = 1000;
 		Thread[] threads = new Thread[num];
 		for (int i = 0; i < num; i++) {
 			threads[i] = new Visitor();
